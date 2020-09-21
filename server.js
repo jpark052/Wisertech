189ce715b0db30f7356975abdafa4fc32bef0e9e
Jae Park <jaepark052@gmail.com>
2020-09-21 16:04:05 -0700

    Add comments

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
+"use strict";
 const express = require('express');
 const path = require('path');
 const app = express();
 const imageToBase64 = require('image-to-base64');
 const WebSocket = require('ws');
 const fs = require("fs");
+
 const { StillCamera } = require("pi-camera-connect");
 const stillCamera = new StillCamera();
 const port = 8765;
-//var server = app.listen(port, "10.0.0.223"); // probably not the right ip :(
-var base64
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`listening http://localhost:${port}`);
+  console.log(`Server is listening at port: ${port}`);
 });
 
 const socketServer = new WebSocket.Server({ port: 3030 });
 
-const messages = ['Start Chatting!'];
-
-socketServer.on('connection', (socketClient) => {
+socketServer.on('connection', (ws) => {
   console.log('connected');
   console.log('client Set length: ', socketServer.clients.size);
-  socketClient.send(JSON.stringify(messages));
 
-  socketClient.on('message', (message) => {
+  ws.on('message', (message) => {
+
+    /* 
+     * function takes() is triggered when server receives a message from the client
+     * i. e. when user press the button
+     * It takes the picture, saves it in the folder 'pics', format it in base64 string and send it to client 
+   */
     function takes() {
-stillCamera.takeImage().then(image => {
-fs.writeFileSync(`/home/pi/pics/testing.jpg`, image);
-       
+      stillCamera.takeImage().then(image => {
+        fs.writeFileSync(`/home/pi/pics/testing.jpg`, image);
+        
+        let base64
         imageToBase64("/home/pi/pics/testing.jpg") // Path to the image
-    .then((response) => {
+          .then((response) => {
             base64 = response
-        })
-    .catch((error) => {
+          })
+          .catch((error) => {
             console.log(error); // Logs an error if there was one
-        })
-       
-        socketClient.send(base64)
-})
-}
+          })
+        ws.send(base64)
+      })
+    }
     setInterval(takes, 5000);
   });
 
-  socketClient.on('close', (socketClient) => {
+  ws.on('close', (socketClient) => {
     console.log('closed');
     console.log('Number of clients: ', socketServer.clients.size);
   });
