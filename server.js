e1726066d0adf3525501ec91e3f9b1843fda10b9
Jae Park <jaepark052@gmail.com>
2020-09-21 13:33:14 -0700

    cleaning out

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const imageToBase64 = require('image-to-base64');
+const WebSocket = require('ws');
+const fs = require("fs");
+const { StillCamera } = require("pi-camera-connect");
+const stillCamera = new StillCamera();
+const port = 8765;
+//var server = app.listen(port, "10.0.0.223"); // probably not the right ip :(
+var base64
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`listening http://localhost:${port}`);
+});
+
+const socketServer = new WebSocket.Server({ port: 3030 });
+
+const messages = ['Start Chatting!'];
+
+socketServer.on('connection', (socketClient) => {
+  console.log('connected');
+  console.log('client Set length: ', socketServer.clients.size);
+  socketClient.send(JSON.stringify(messages));
+
+  socketClient.on('message', (message) => {
+    function takes() {
+stillCamera.takeImage().then(image => {
+fs.writeFileSync(`/home/pi/pics/testing.jpg`, image);
+       
+        imageToBase64("/home/pi/pics/testing.jpg") // Path to the image
+    .then((response) => {
+            base64 = response
+        })
+    .catch((error) => {
+            console.log(error); // Logs an error if there was one
+        })
+       
+        socketClient.send(base64)
+})
+}
+    setInterval(takes, 5000);
+  });
+
+  socketClient.on('close', (socketClient) => {
+    console.log('closed');
+    console.log('Number of clients: ', socketServer.clients.size);
+  });
+});
