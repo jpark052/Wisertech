84958fa2c3798469ed790477766e3c636fbe7c72
Jae Park <jaepark052@gmail.com>
2020-10-01 13:25:48 -0700

    Added a separate page for stream

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 const cmd = require("node-cmd")
 const port = 8765
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
+
 
 app.listen(port, () => {
   console.log(`Server is listening at port: ${port}`)
@@ -34,13 +37,21 @@ socketServer.on('connection', (ws) => {
             }
              
         });
-    ws.send('streaming started')
+    //ws.send('streaming started')
   })
 
   ws.on('close', (socketClient) => {
-    clearInterval(interval)
     console.log('closed')
     console.log('Number of clients: ', socketServer.clients.size)
+    // need to shut down the streamer only if there's no client left
+    if (socketServer.clients.size == 0) {
+    cmd.get(`kill $(pgrep mjpg_streamer) > /dev/null 2>&1`, function(err, data, stderr){
+               if (err) {
+               console.log(err)
+            }
+            //console.log('success')
+        })
+      }
   })
 })
 
