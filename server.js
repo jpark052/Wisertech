d9e25e2c40b13fadcb4880c5b4b38249cc530b97
Jae Park <jaepark052@gmail.com>
2020-10-02 10:15:17 -0700

    format & comments added

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const path = require('path');
 const WebSocket = require('ws')
-const fs = require("fs")
 const app = express();
 const cmd = require("node-cmd")
 const port = 8765
@@ -13,7 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-
 app.listen(port, () => {
   console.log(`Server is listening at port: ${port}`)
 })
@@ -23,38 +21,34 @@ const socketServer = new WebSocket.Server({ port: 3030 })
 socketServer.on('connection', (ws) => {
   console.log('connected')
   console.log('client Set length: ', socketServer.clients.size)
-  
+
   ws.on('message', (message) => {
     console.log('we have received a request')
-    
+
     // executing the mjpg_streamer
-    
     cmd.get(`cd /home/pi/Wisertech/mjpg-streamer/mjpg-streamer-experimental
              export LD_LIBRARY_PATH=.
              mjpg_streamer -i "./input_uvc.so -n -f 30 -r 640x480 -d /dev/video0"  -o "./output_http.so -w ./www"`,
-        function(err, data, stderr){
-               if (err) {
-               console.log('error: ', err)
-            }
-             
-        })
-      
-    //ws.send('streaming started')
+      function (err, data, stderr) {
+        if (err) {
+          console.log('error: ', err)
+        }
+      })
   })
 
   ws.on('close', (socketClient) => {
     console.log('closed')
     console.log('Number of clients: ', socketServer.clients.size)
-    
+
     // shut down the streamer only if there's no client left
     if (socketServer.clients.size == 0) {
       //console.log('zero!')
-      cmd.get(`kill $(pgrep mjpg_streamer) > /dev/null 2>&1`, function(err, data, stderr){
-              if (err) {
-              console.log(err)
-          }
-        })
-      }
+      cmd.get(`kill $(pgrep mjpg_streamer) > /dev/null 2>&1`, function (err, data, stderr) {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
   })
 })
 
