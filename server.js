6a1f546c52dd0f722c7ef015c8c23a2d07789513
Jae Park <jaepark052@gmail.com>
2020-10-05 13:17:31 -0700

    added more comments

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+/**
+ * Video streaming server using express/websocket.
+ * mjpg_streamer is used for streaming fucntion.
+ * 
+ */
+
 "use strict"
 const express = require('express')
 const path = require('path');
 const WebSocket = require('ws')
-const app = express();
-const cmd = require("node-cmd")
+const app = express();  // initialize express server
+const cmd = require("node-cmd") // cmd package is required to execute shell command
 const port = 8765
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))  // using HTML files in 'public' folder
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
@@ -16,16 +22,17 @@ app.listen(port, () => {
   console.log(`Server is listening at port: ${port}`)
 })
 
+// initialize websocket server
 const socketServer = new WebSocket.Server({ port: 3030 })
 
 socketServer.on('connection', (ws) => {
   console.log('connected')
-  console.log('client Set length: ', socketServer.clients.size)
+  console.log('client Set length: ', socketServer.clients.size) // showing the number of clients connected when initialize
 
   ws.on('message', (message) => {
     console.log('we have received a request')
 
-    // executing the mjpg_streamer
+    // executing the mjpg_streamer. It will be displayed in 'stream.html' page
     cmd.get(`cd /home/pi/Wisertech/mjpg-streamer/mjpg-streamer-experimental
              export LD_LIBRARY_PATH=.
              mjpg_streamer -i "./input_uvc.so -n -f 30 -r 1280x960 -d /dev/video0"  -o "./output_http.so -w ./www"`,
