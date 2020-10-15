8a308d80a638378f15ee94737b12824dbbc1796f
Jae Park <jaepark052@gmail.com>
2020-10-15 10:13:46 -0700

    added configuration change function

--- ui/server.js
+++ ui/server.js
@@ -14,17 +14,12 @@ const fs = require('fs')
 const WebSocket = require('ws')
 
 let graphJSON
+let userJSON
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
-})
-
-app.get('/graph', (req, res) => {
-    res.write(path.join(__dirname, 'graph.html'))
-    res.write("sup")
-    //res.send("helloooooo")
 })
 
 app.listen(3000, () => console.log('Server Started'))
@@ -35,13 +30,13 @@ socketServer.on('connection', (ws) => {
     console.log('connected')
 
     // sending userInfo/configuration JSON to client when connected
-    fs.readFile('./client.json', 'utf8', (err, jsonString) => {
+    fs.readFile('./public/client.json', 'utf8', (err, jsonString) => {
         if (err) {
             console.log("File read failed:", err)
             return
         }
-        const jsonData = JSON.parse(jsonString)
-        const stringData = JSON.stringify(jsonData)
+        userJSON = JSON.parse(jsonString)
+        const stringData = JSON.stringify(userJSON)
         ws.send(stringData)
     })
 
@@ -51,32 +46,64 @@ socketServer.on('connection', (ws) => {
             return
         }
         graphJSON = JSON.parse(jsonString)
-        send5sec()
     })
 
 
     // send JSON object to the client every 5 second
-    function send5sec() {
-        var dataArray = [Math.floor((Math.random() * 20) + 1),
-        Math.floor((Math.random() * 20) + 1),
-        Math.floor((Math.random() * 20) + 1),
-        Math.floor((Math.random() * 20) + 1),
-        Math.floor((Math.random() * 20) + 1),
-        Math.floor((Math.random() * 20) + 1),
-        Math.floor((Math.random() * 20) + 1)]
-        graphJSON.graphData.data.datasets[0].data = dataArray
-    
-        ws.send(JSON.stringify(graphJSON))
-        setTimeout(send5sec, 5000)
-    }
+
 
     ws.on('message', (message) => {
         console.log(`we have received a message: `)
         console.log(JSON.parse(message))
+        let received = JSON.parse(message)
 
         // updating the most recent user configuration
-        fs.writeFile('./client.json', message, (err) => {
-            if (err) console.log("Error: ", err)
-        })
+        if (received.dataType == "userInfo") {
+            fs.writeFile('./client.json', message, (err) => {
+                if (err) console.log("Error: ", err)
+            })
+        } else if (received.dataType == "graphData"){
+            fs.writeFile('./clientGraph.json', message, (err) => {
+                if (err) console.log("Error: ", err)
+            })
+        } else if (received.dataType == "configuration"){
+            userJSON.configuration.videoQuality = received.videoQuality
+            let amplitude = received.amplitude
+        } else if (received.dataType == "videoTrigger"){
+            //command to stream video
+
+        }
     })
 })
+
+fs.readFile('./public/clientGraph.json', 'utf8', (err, jsonString) => {
+    if (err) {
+        console.log("File read failed:", err)
+        return
+    }
+    graphJSON = JSON.parse(jsonString)
+    console.log(graphJSON)
+
+    renew5sec();
+
+})
+
+function renew5sec() {
+
+    var dataArray = [Math.floor((Math.random() * 20) + 30),
+    Math.floor((Math.random() * 100) + 1),
+    Math.floor((Math.random() * 100) + 1),
+    Math.floor((Math.random() * 100) + 1),
+    Math.floor((Math.random() * 100) + 1),
+    Math.floor((Math.random() * 100) + 1),
+    Math.floor((Math.random() * 100) + 1)]
+    graphJSON.graphData.data.datasets[0].data = dataArray
+
+    //let newGraph = JSON.stringify(graphJSON)
+
+    fs.writeFile('./public/clientGraph.json', JSON.stringify(graphJSON, null, "\t"), (err) => {
+        if (err) console.log("Error: ", err)
+    })
+
+    setTimeout(renew5sec, 1000)
+}
