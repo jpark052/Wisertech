30ea7f0825c4de579d5b555e979308f05e0c6c4b
Jae Park <jaepark052@gmail.com>
2020-10-30 13:21:45 -0700

    cc

--- variables.js
+++ variables.js
@@ -1,7 +1,9 @@
-const cmd = require("node-cmd")
- cmd.get(`ip route get 1.2.3.4 | awk '{print $7}'`, function (err, data, stderr) {
-        if (err) {
-          console.log('error: ', err)
-        }
-        const ipAddress = data
-      })
+// const cmd = require("node-cmd")
+//  cmd.get(`ip route get 1.2.3.4 | awk '{print $7}'`, function (err, data, stderr) {
+//         if (err) {
+//           console.log('error: ', err)
+//         }
+//         const ipAddress = data
+//       })
+
+const ipAddress = 1223
