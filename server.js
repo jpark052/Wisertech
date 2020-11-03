4c8c874ff2297635297caae713d052592cb0bf49
Jae Park <jaepark052@gmail.com>
2020-11-03 11:09:36 -0800

    fixed

--- server.js
+++ server.js
@@ -88,8 +88,7 @@ socketServer.on('connection', (ws) => {
       
       // executing the mjpg_streamer. It will be displayed in 'stream.html' page
       cmd.get(`cd /home/pi/Wisertech/mjpg-streamer/mjpg-streamer-experimental
-    export LD_LIBRARY_PATH=.
-    mjpg_streamer -i "./input_uvc.so -n -f 30 -r 640x480 -d /dev/video0"  -o "./output_http.so -w ./www"`,
+      ./mjpg_streamer -i "./input_uvc.so" -o "./output_http.so -w ./www"`,
 
         function (err, data, stderr) {
           if (err) {
